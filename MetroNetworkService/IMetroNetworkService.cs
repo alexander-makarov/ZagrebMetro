3f39708f14d62624f3b2b8fe0e90e32839536d97
Alexander Makarov <makarov.dev@gmail.com>
2015-10-23 06:31:15 +0300

    ZagrebMetro REST JSON service scaffold

---
+++ MetroNetworkService/IMetroNetworkService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+
+namespace MetroNetworkService
+{
+    [ServiceContract(Namespace = "http://www.corvus.hr/ZagrebMetro/2015/10")]
+    public interface IMetroNetworkService
+    {
+        [OperationContract]
+        string GetTripDistance(IEnumerable<string> stationsList);
+    }
+}
---
+++ ZagrebMetroService/ZagrebMetro.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel.Web;
+using MetroNetwork;
+using MetroNetworkService;
+
+namespace ZagrebMetroService
+{
+    public class ZagrebMetro : IMetroNetworkService
+    {
+        private readonly MetroNetworkGraph _metroNetwork;
+
+        public ZagrebMetro(MetroNetworkGraph metroNetwork)
+        {
+            _metroNetwork = metroNetwork;
+        }
+
+        [WebInvoke(Method = "POST",
+                    ResponseFormat = WebMessageFormat.Json,
+                    UriTemplate = "trip/distance/{stationsList}")]
+        public string GetTripDistance(IEnumerable<string> stationsList)
+        {
+            return "Got it!";
+        }
+    }
+}
